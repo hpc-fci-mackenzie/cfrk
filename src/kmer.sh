54ec1e5dcb70075c2248c2c88a149c806e0f2ade
Fabricio <fabricio@jerico.labinfo1.lncc.br>
2016-06-09 15:06:02 -0300

    Implementado scripts para execução com Swift.

--- src/kmer.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for i in "AA" "AC" "AG" "AT" "CA" "CC" "CG" "CT" "GA" "GC" "GG" "GT" "TA" "TC" "TG" "TT"
-do
-   echo ${i}
-   grep -c ${i} ../seq/SRR2155482.fasta_0.fasta
-done
---
+++ swift/roda.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+swift cfrk.swf -n=2 -k=2 -dataset-dir=/home/fabricio/ncbi/public/sra/ERR1293898 -dataset-prefix=ERR1293898 -out-dir=../result
