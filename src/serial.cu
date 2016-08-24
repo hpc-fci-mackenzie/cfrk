887cec640cea921e64289930f41b1a4ebea43ea9
fabricio <fabricio@prjcuda11.lncc.br>
2016-08-24 08:57:04 -0300

    Modificação no código serial.

--- src/serial.cu
+++ src/serial.cu
@@ -11,9 +11,7 @@ void ComputeIndex(short *S, int *M, const ushort k, lint nN)
 
    for (i = 0; i < nN; i++)
    {
-
       index = 0;
-
       for (j = 0; j < k; j++)
       {
          if (S[j + i] != -1 && i < nN-(k-1)) //Verifica se ha alguem que nao deve ser processado
@@ -26,9 +24,7 @@ void ComputeIndex(short *S, int *M, const ushort k, lint nN)
             break;
          }
       }//End for j
-
       M[i] = index;
-
    }//End for i
 }
 
@@ -83,15 +79,60 @@ void PrintFreq(ushort *Freq, lint nS, ushort fourk)
    }
 }
 
+struct read* SelectChunk(struct read *rd, ushort chunkSize, ushort it, lint max, lint gnS, lint *nS, lint gnN, lint *nN)
+{
+   struct read *chunk;
+   int i;
+   lint length = 0;
+
+   // Size to be allocated
+   for (i = 0; i < max; i++)
+   {
+      int id = chunkSize*it + i;
+      if (id > gnS-1)
+      {
+         break;
+      }
+      //printf("id: %d\n", id);
+      length += rd->length[id]+1;
+   }
+
+   cudaMallocHost((void**)&chunk, sizeof(struct read));
+   cudaMallocHost((void**)&chunk->data, sizeof(short)*length);
+   cudaMallocHost((void**)&chunk->length, sizeof(int)*chunkSize);
+   cudaMallocHost((void**)&chunk->start, sizeof(int)*chunkSize);
+
+   // Copy rd->data to chunk->data
+   lint start = rd->start[chunkSize*it];
+   lint end = start + length;
+   for (i = start; i < end; i++)
+   {
+      chunk->data[i-start] = rd->data[i];
+   }
+
+   chunk->length[0] = rd->length[chunkSize*it];
+   chunk->start[0] = 0;
+   // Copy start and length
+   for (i = 1; i < max; i++)
+   {
+      int id = chunkSize*it + i;
+      chunk->length[i] = rd->length[id];
+      chunk->start[i] = chunk->start[i-1]+(chunk->length[i-1]+1);
+   }
+
+   *nN = length;
+   *nS = max;
+return chunk;
+}
+
 int main(int argc, char *argv[])
 {
 
-   long int nN, nS;
+   long int gnN, gnS, nS, nN;
    struct read *rd;
-   int *M;
-   ushort *Freq1;//, *Freq2;
-   //double **M_Freq;
-   size_t size1, size2;//, size3;
+   int *Index;
+   ushort *Freq;;
+   size_t size1, size2;
 
    if( argc < 2 )
    {
@@ -100,37 +141,50 @@ int main(int argc, char *argv[])
    }
 
    int k = atoi( argv[2] ); if( k <= 0 ) return 2;
+   int fourk = POW(k);
 
    puts("\n\t\tReading seqs!!!");
    rd = (struct read*)malloc(sizeof(struct read));
-   ReadFASTASequences(argv[1], &nN, &nS, rd, 0);
-   printf("\nnS: %ld, nN: %ld\n", nS, nN);
+   ReadFASTASequences(argv[1], &gnN, &gnS, rd, 0);
+   printf("\nnS: %ld, nN: %ld\n", gnS, gnN);
 
-   size1 = nN * sizeof(int);// M size
-   size2 = pow(4,k) * nS * sizeof(ushort);// Freq size
-   //size3 = pow(4,k+1) * nS * sizeof(ushort);// Freq size with k+1
-   
-   M = (int*)malloc(size1);
-   Freq1 = (ushort*)malloc(size2);
-   //Freq2 = (ushort*)malloc(size3);
 
-   memset(M, -1, size1);
+   int chunkSize = 4096;
+   int nChunk = floor(gnS/chunkSize);
+   struct read *chunk;
+   int i = 0;
+   for (i = 0; i < nChunk; i++)
+   {
+      chunk = SelectChunk(rd, chunkSize, i, chunkSize, gnS, &nS, gnN, &nN);
+      size1 = nN * sizeof(int);// Index size
+      size2 = pow(4,k) * nS * sizeof(ushort);// Freq size
+      Index = (int*)malloc(size1);
+      Freq = (ushort*)malloc(size2);
 
-   ComputeIndex(rd->data, M, k, nN);
+      memset(Index, -1, size1);
 
-   //Print(M, nN);
+      ComputeIndex(chunk->data, Index, k, nN);
+      ComputeFreq(Index, Freq, chunk->start, chunk->length, fourk, nS);
+      cudaFree(chunk->data);
+      cudaFree(chunk->length);
+      cudaFree(chunk->start);
+      cudaFree(chunk);
+   }
+   int chunkRemain = abs(gnS - (nChunk*chunkSize));
+   chunk = SelectChunk(rd, chunkSize, nChunk, chunkRemain, gnS, &nS, gnN, &nN);
+   ComputeIndex(chunk->data, Index, k, nN);
+   ComputeFreq(Index, Freq, chunk->start, chunk->length, fourk, nS);
+
+   //Print(Index, nN);
 
    ushort fourk1 = pow(4,k);
-   ComputeFreq(M, Freq1, rd->start, rd->length, fourk1, nS);
    //ushort fourk2 = pow(4,k+1);
    //ComputeFreq(M, Freq2, rd->start, rd->length, fourk2, nS);
 
-/*
-   printf("Freq1:\n");
-   PrintFreq(Freq1, nS, fourk1);
-   printf("Freq2:\n");
-   PrintFreq(Freq2, nS, fourk2);
-*/
+
+   //printf("Freq1:\n");
+   //PrintFreq(Freq, nS, fourk);
+
    printf("\nfim\n");
 
 return 0;
