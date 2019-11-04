37c6e9931797f061a804346c68c767edea819a55
Matheus Pedro Rodrigues Ferreira <matheusprf95@gmail.com>
2019-11-04 20:15:46 -0300

    ~ Organizing files
    Signed-off-by: Matheus Pedro Rodrigues Ferreira <matheusprf95@gmail.com>

---
+++ src/kmer_main.cu
@@ -0,0 +1,128 @@
+#include <stdio.h>
+#include <cuda.h>
+#include <math.h>
+#include <stdint.h>
+#include <string.h>
+#include "tipos.h"
+#include "kmer.cuh"
+
+void GetDeviceProp(uint8_t device, lint *maxGridSize, lint *maxThreadDim, lint *deviceMemory)
+{
+   cudaDeviceProp prop;
+
+   cudaGetDeviceProperties(&prop, device);
+
+   *maxThreadDim = prop.maxThreadsDim[0];
+   *maxGridSize = prop.maxGridSize[0];
+   *deviceMemory = prop.totalGlobalMem;
+}
+
+void kmer_main(struct read *rd, lint nN, lint nS, int k, ushort device, cudaStream_t *stream)
+{
+
+   int *d_Index;// Index vector
+   char *d_Seq;// Seq matrix
+   // int *Freq;
+   int *d_Freq;// Frequence vector
+   int fourk;// 4 power k
+   lint *d_start;
+   int *d_length;// The beggining and the length of each sequence
+   lint block[4], grid[4];// Grid config; 0:nN, 1:nS
+   lint maxGridSize, maxThreadDim, deviceMemory;// Device config
+   ushort offset[4] = {1,1,1,1};
+   size_t size[5], totalsize;
+
+   d_Index =  NULL;
+   d_Seq = NULL;
+
+   fourk = POW(k);
+
+   cudaSetDevice(device);
+   GetDeviceProp(device, &maxGridSize, &maxThreadDim, &deviceMemory);
+
+//---------------------------------------------------------------------------
+   size[0] = nN * sizeof(char);// d_Seq and Seq size
+   size[1] = nN * sizeof(int); // d_Index and Index size
+   size[2] = nS * sizeof(int);  // d_length
+   size[3] = nS * fourk * sizeof(int);// Freq and d_Freq
+   size[4] = nS * sizeof(lint); // d_start
+   totalsize = size[0] + size[1] + (size[2] * 2) + size[3];
+
+   if (totalsize > deviceMemory)
+   {
+      printf("\n\n\t\t\t[Error] There is no enough space on GPU memory\n");
+      printf("\t\t\t[Error] Required memory: %ld; Available memory: %ld\n", totalsize, deviceMemory);
+      exit(1);
+   }
+//---------------------------------------------------------------------------
+
+   if ( cudaMallocHost ((void**)&d_Seq, size[0])    != cudaSuccess ) printf("\n[Error 1] %s\n", cudaGetErrorString(cudaGetLastError()));
+   if ( cudaMallocHost ((void**)&d_Index, size[1])  != cudaSuccess ) printf("\n[Error 2] %s\n", cudaGetErrorString(cudaGetLastError()));
+   if ( cudaMallocHost ((void**)&d_start, size[4])  != cudaSuccess ) printf("\n[Error 3] %s\n", cudaGetErrorString(cudaGetLastError()));
+   if ( cudaMallocHost ((void**)&d_length, size[2]) != cudaSuccess ) printf("\n[Error 4] %s\n", cudaGetErrorString(cudaGetLastError()));
+   if ( cudaMallocHost ((void**)&d_Freq, size[3])   != cudaSuccess ) printf("\n[Error 5] %s\n", cudaGetErrorString(cudaGetLastError()));
+
+//************************************************
+   block[0] = maxThreadDim;
+   grid[0] = floor(nN / block[0]) + 1;
+   if (grid[0] > maxGridSize)
+   {
+      grid[0] = maxGridSize;
+      offset[0] = (nN / (grid[0] * block[0])) + 1;
+   }
+
+   block[1] = maxThreadDim;
+   grid[1] = (nS / block[1]) + 1;
+   if (grid[1] > maxGridSize)
+   {
+      grid[1] = maxGridSize;
+      offset[1] = (nS / (grid[1] * block[1])) + 1;
+   }
+
+   block[2] = maxThreadDim;
+   grid[2] = nS;
+   if (nS > maxGridSize)
+   {
+      grid[2] = maxGridSize;
+      offset[2] = (nS / grid[2]) + 1;
+   }
+
+   int nF = nS*POW(k);
+   block[3] = maxThreadDim;
+   grid[3] = ((nS*POW(k))/1024)+1;
+   if (grid[3] > maxGridSize)
+   {
+      grid[3] = maxGridSize;
+      offset[3] = (nF / (grid[3] * block[3])) + 1;
+   }
+
+//************************************************
+
+   if ( cudaMemcpyAsync(d_Seq, rd->data, size[0], cudaMemcpyHostToDevice, *stream) != cudaSuccess) printf("[Error 6] %s\n", cudaGetErrorString(cudaGetLastError()));
+   if ( cudaMemcpyAsync(d_start, rd->start, size[4], cudaMemcpyHostToDevice, *stream) != cudaSuccess) printf("[Error 7] %s\n", cudaGetErrorString(cudaGetLastError()));
+   if ( cudaMemcpyAsync(d_length, rd->length, size[2], cudaMemcpyHostToDevice, *stream) != cudaSuccess) printf("[Error 8] %s\n", cudaGetErrorString(cudaGetLastError()));
+
+//************************************************
+
+   SetMatrix<<<grid[0], block[0], 0, *stream>>>(d_Index, offset[0], -1, nN);
+   SetMatrix<<<grid[3], block[3], 0, *stream>>>(d_Freq, offset[3], 0, nF);
+   ComputeIndex<<<grid[0], block[0], 0, *stream>>>(d_Seq, d_Index, k, nN, offset[0]);
+   //ComputeFreq<<<grid[1], block[1]>>>(d_Index, d_Freq, d_start, d_length, offset[1], fourk, nS, nN);
+   ComputeFreqNew<<<grid[2],block[2], 0, *stream>>>(d_Index, d_Freq, d_start, d_length, offset[2], fourk, nS);
+
+   //cudaFree(rd);
+
+   if ( cudaMallocHost(&rd->Freq, size[3]) != cudaSuccess) printf("\n[Error 9] %s\n", cudaGetErrorString(cudaGetLastError()));
+   if ( cudaMemcpyAsync(rd->Freq, d_Freq, size[3], cudaMemcpyDeviceToHost, *stream) != cudaSuccess) printf("\n[Error 10] %s\n", cudaGetErrorString(cudaGetLastError()));
+
+
+//************************************************
+   cudaFree(d_Seq);
+   cudaFree(d_Freq);
+   cudaFree(d_Index);
+   cudaFree(d_start);
+   cudaFree(d_length);
+//---------------------------------------------------------------------------
+
+   //printf("\nFim kmer_main\n");
+}
---
+++ src/main.cu
@@ -0,0 +1,318 @@
+/*
+CFRK-MT - Contabilizador da Frequencia de Repetica de kmer (Multi GPU version)
+Developer: Fabricio Gomes Vilasboas
+Istitution: National Laboratory for Scientific Computing
+*/
+
+#include <stdio.h>
+#include <pthread.h>
+#include <math.h>
+#include <stdint.h>
+#include <omp.h>
+#include <string.h>
+#include "kmer.cuh"
+#include "tipos.h"
+#include "fastaIO.h"
+
+//*** Global Variables ***
+struct read *chunk;
+lint *nS, *nN;
+int offset;
+int device;
+int k;
+char file_out[512];
+//************************
+
+void PrintFreq(struct seq *seq, struct read *pchunk, int nChunk, int chunkSize)
+{
+   FILE *out;
+   int cont = 0;
+   int cont_seq = 0;
+   char str[32];
+   lint fourk = pow(4,k);
+
+   out = fopen(file_out, "w");
+
+   for (int j = 0; j < nChunk; j++)
+   {
+      cont = 0;
+      cont_seq = 0;
+      for (int i = 0; i < (chunkSize*fourk); i++)
+      {
+         if (i % fourk == 0)
+         {
+            cont = 0;
+            sprintf(str, "\n");
+            // printf("%s", str);
+            fwrite(str, sizeof(char), sizeof(str), out);
+            // printf("%s", seq[cont_seq].header);
+            cont_seq++;
+         }
+         if (pchunk[j].Freq[i] != 0)
+         {
+            sprintf(str, "%d ", pchunk[j].Freq[i]);
+            // printf("%s", str);
+            fwrite(str, sizeof(char), sizeof(str), out);
+         }
+         cont++;
+      }
+   }
+   fclose(out);
+}
+
+void DeviceInfo(uint8_t device)
+{
+   cudaDeviceProp prop;
+
+   cudaGetDeviceProperties(&prop, device);
+
+   printf("\n\n***** Device information *****\n\n");
+
+   printf("\tId: %d\n", device);
+   printf("\tName: %s\n", prop.name);
+   printf("\tTotal global memory: %ld\n", prop.totalGlobalMem);
+   printf("\tMax grid size: %d, %d, %d\n", prop.maxGridSize[0], prop.maxGridSize[1], prop.maxGridSize[2]);
+   printf("\tMax thread dim: %d, %d, %d\n", prop.maxThreadsDim[0], prop.maxThreadsDim[1], prop.maxThreadsDim[2]);
+   printf("\tWarp size: %d\n", prop.warpSize);
+   printf("\tMax threads per multiprocessor: %d\n", prop.maxThreadsPerMultiProcessor);
+
+   printf("\n************************************\n\n");
+}
+
+int SelectDevice(int devCount)
+{
+
+   int i, device = 0;
+   cudaDeviceProp prop[devCount];
+
+   if (devCount > 0)
+   {
+      for (i = 0; i < devCount; i++)
+      {
+         cudaGetDeviceProperties(&prop[i], i);
+      }
+
+      for (i = 0; i < devCount; i++)
+      {
+         if (prop[i].totalGlobalMem > prop[device].totalGlobalMem)
+         {
+            device = i;
+         }
+      }
+   }
+   else
+      return 0;
+
+return device;
+}
+
+struct read* SelectChunkRemain(struct read *rd, ushort chunkSize, ushort it, lint max, lint gnS, lint *nS, lint gnN, lint *nN, int nt)
+{
+   struct read *chunk;
+   lint i;
+   lint j;
+   lint length = 0;
+
+   // Size to be allocated
+   for (i = 0; i < max; i++)
+   {
+      lint id = chunkSize*it + i;
+      if (id > gnS-1)
+      {
+         break;
+      }
+      length += rd->length[id]+1;
+   }
+
+   cudaMallocHost((void**)&chunk, sizeof(struct read));
+   cudaMallocHost((void**)&chunk->data, sizeof(char)*length);
+   cudaMallocHost((void**)&chunk->length, sizeof(int)*chunkSize);
+   cudaMallocHost((void**)&chunk->start, sizeof(lint)*chunkSize);
+
+   // Copy rd->data to chunk->data
+   lint start = rd->start[chunkSize*it];
+   lint end = start + (lint)length;
+
+   #pragma omp parallel for num_threads(nt)
+   for (j = start; j < end; j++)
+   {
+      chunk->data[j-start] = rd->data[j];
+   }
+
+   chunk->length[0] = rd->length[chunkSize*it];
+   chunk->start[0] = 0;
+
+   // Copy start and length
+   for (i = 1; i < max; i++)
+   {
+      lint id = chunkSize*it + i;
+      chunk->length[i] = rd->length[id];
+      chunk->start[i] = chunk->start[i-1]+(chunk->length[i-1]+1);
+   }
+
+   *nN = length;
+   *nS = max;
+return chunk;
+}
+
+
+void SelectChunk(struct read *chunk, const int nChunk, struct read *rd, ushort chunkSize, lint max, lint gnS, lint *nS, lint gnN, lint *nN, int nt)
+{
+   lint i, j, it;
+
+   for (it = 0; it < nChunk; it++)
+   {
+      lint length = 0;
+
+      // Size to be allocated
+      for (i = 0; i < max; i++)
+      {
+         lint id = chunkSize*it + i;
+         if (id > gnS-1)
+         {
+            break;
+         }
+         length += rd->length[id]+1;
+      }
+
+      cudaMallocHost((void**)&chunk[it].data, sizeof(char)*length);
+      cudaMallocHost((void**)&chunk[it].length, sizeof(int)*chunkSize);
+      cudaMallocHost((void**)&chunk[it].start, sizeof(lint)*chunkSize);
+
+      // Copy rd->data to chunk->data
+      lint start = rd->start[chunkSize*it];
+      lint end = start + (lint)length;
+      #pragma omp parallel for num_threads(nt)
+      for (j = start; j < end; j++)
+      {
+         chunk[it].data[j-start] = rd->data[j];
+      }
+
+      chunk[it].length[0] = rd->length[chunkSize*it];
+      chunk[it].start[0] = 0;
+
+      // Copy start and length
+      for (i = 1; i < max; i++)
+      {
+         lint id = chunkSize*it + i;
+         chunk[it].length[i] = rd->length[id];
+         chunk[it].start[i] = chunk[it].start[i-1]+(chunk[it].length[i-1]+1);
+      }
+
+      nN[it] = length;
+      nS[it] = max;
+   }
+}
+
+void *LaunchKmer(void* threadId)
+{
+
+   lint tid = (lint)threadId;
+
+   int start = tid * offset;
+   int end = start+offset;
+
+   printf("\t\ttid: %d\n", tid);
+
+   DeviceInfo(tid);
+   cudaStream_t stream[end - start];
+   int i = 0;
+   for (i = start; i < end; i++)
+   {
+      cudaStreamCreate(&stream[i]);
+      kmer_main(&chunk[i], nN[i], nS[i], k, device, &stream[i]);
+      cudaStreamSynchronize(0);
+      cudaFreeHost(chunk[i].data);
+      cudaFreeHost(chunk[i].length);
+      cudaFreeHost(chunk[i].start);
+      cudaStreamDestroy(stream[i]);
+   }
+
+return NULL;
+}
+
+int main(int argc, char* argv[])
+{
+
+   lint gnN, gnS, chunkSize = 8192;
+   int devCount;
+   int nt = 12;
+
+   if ( argc < 4)
+   {
+      printf("Usage: ./cfrk [dataset.fasta] [file_out.cfrk] [k] <number of threads: Default 12> <chunkSize: Default 8192>");
+      return 1;
+   } 
+   cudaDeviceReset();
+   
+   k = atoi(argv[3]);
+   if (argc == 5)
+      nt = atoi(argv[4]);
+   if (argc == 6)
+      chunkSize = atoi(argv[5]);
+
+   cudaGetDeviceCount(&devCount);
+   DeviceInfo(device);
+
+   strcpy(file_out, argv[2]);
+
+   printf("\ndataset: %s, out: %s, k: %d, chunkSize: %d\n", argv[1], file_out, k, chunkSize);
+
+   lint st = time(NULL);
+   puts("\n\n\t\tReading seqs!!!");
+   struct read *rd;
+   cudaMallocHost((void**)&rd, sizeof(struct read));
+//   rd = (struct read*)malloc(sizeof(struct read));
+   struct seq *seq = ReadFASTASequences(argv[1], &gnN, &gnS, rd, 1);
+   printf("\nnS: %ld, nN: %ld\n", gnS, gnN);
+   lint et = time(NULL);
+
+   printf("\n\t\tReading time: %ld\n", (et-st));
+
+   int nChunk = floor(gnS/chunkSize);
+   int i = 0;
+   cudaMallocHost((void**)&chunk, sizeof(struct read)*nChunk);
+   cudaMallocHost((void**)&nS, sizeof(lint)*nChunk);
+   cudaMallocHost((void**)&nN, sizeof(lint)*nChunk);
+   SelectChunk(chunk, nChunk, rd, chunkSize, chunkSize, gnS, nS, gnN, nN, nt);
+
+   device = SelectDevice(devCount);
+   offset = floor(nChunk/devCount);
+   pthread_t threads[devCount];
+
+   for (i = 0; i < devCount; i++)
+   {
+      pthread_create(&threads[i], NULL, LaunchKmer, (void*) &i);
+   }
+
+   for (i = 0; i < devCount; i++)
+   {
+
+      pthread_join(threads[i], NULL);
+   }
+
+   int threadRemain = nChunk - (offset*devCount);
+   if (threadRemain > 0)
+   {
+       cudaStream_t stream[1];
+       cudaStreamCreate(&stream[0]);
+      kmer_main(&chunk[nChunk-1], nN[nChunk-1], nS[nChunk-1], k, device, &stream[0]);
+       cudaStreamDestroy(stream[0]);
+   }
+
+   int chunkRemain = abs(gnS - (nChunk*chunkSize));
+   lint rnS, rnN;
+   struct read *chunk_remain = SelectChunkRemain(rd, chunkSize, nChunk, chunkRemain, gnS, &rnS, gnN, &rnN, nt);
+    cudaStream_t stream[1];
+    cudaStreamCreate(&stream[0]);
+   kmer_main(chunk_remain, rnN, rnS, k, device, &stream[0]);
+    cudaStreamDestroy(stream[0]);
+
+   // st = time(NULL);
+   // PrintFreq(seq, chunk, nChunk, chunkSize);
+   // et = time(NULL);
+   // PrintFreq(seq, chunk_remain, 1, rnS);
+   // printf("\n\t\tWriting time: %ld\n", (et-st));
+
+return 0;
+}
