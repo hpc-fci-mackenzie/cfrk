a9a036e01a153a5b00b755412106219eba4ccadc
Matheus Pedro Rodrigues Ferreira <matheusprf95@gmail.com>
2020-02-06 20:05:14 -0300

    ~ Append to previous commit
    Signed-off-by: mrchase95 <matheus_pedro_95@hotmail.com>

---
+++ src/kmer_openacc.cuh
@@ -0,0 +1,16 @@
+#ifndef _kmer_cuh
+#define _kmer_cuh
+
+#include "tipos.h"
+
+void kmer_main(struct read *rd, lint nN, lint nS, int k, ushort device);
+
+__global__ void SetMatrix(int *Mat, ushort offset, int val, int nF);
+
+__global__ void ComputeIndex(char *Seq, int *Index, const int k, lint nN, ushort offset);
+
+__global__ void ComputeFreq(int *Index, int *Freq, lint *start, int *length, ushort offset, int fourk, lint nS, lint nN);
+
+__global__ void ComputeFreqNew(int *Index, int *Freq, lint *start, int *length, ushort offset, int fourk, lint nS);
+
+#endif
